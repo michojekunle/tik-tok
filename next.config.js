238cfbb6ada35e45dc0f459bef99c816b60d822a
AMD <michojekunle1@gmail.com>
2022-10-03 20:21:37 +0100

    mad changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com'
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
