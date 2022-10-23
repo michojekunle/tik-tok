b65528c477e0768fd03eca6878423815ff9863c6
AMD <michojekunle1@gmail.com>
2022-10-23 05:02:51 +0100

    made changes

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -7,6 +7,7 @@ import { SanityAssetDocument } from '@sanity/client';
 import { topics } from '../utils/constants';
 import useAuthStore from '../store/authStore';
 import { client } from '../utils/client';
+import { BASE_URL } from '../utils';
 
 
 const Upload = () => {
@@ -58,7 +59,7 @@ const Upload = () => {
                 topic: category
             }
 
-            await axios.post('http://localhost:3000/api/post', document);
+            await axios.post(`${BASE_URL}api/post`, document);
             router.push('/')
         }
     }
