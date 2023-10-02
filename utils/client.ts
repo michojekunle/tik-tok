3ef1520126432bc85b270ecf0261e338101eb25b
AMD <90222287+michojekunle@users.noreply.github.com>
2023-10-02 15:31:36 +0100

    Update client.ts

--- utils/client.ts
+++ utils/client.ts
@@ -4,6 +4,6 @@ export const client = sanityClient({
   projectId:'nrev6ch1',
   dataset: 'production',
   apiVersion: '2022-03-10',
-  useCdn: false,
+  useCdn: true,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN, 
 });
