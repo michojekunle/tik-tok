8d95fb85991995cda7cb157f56da2c75d0b43aa5
AMD <michojekunle1@gmail.com>
2023-11-23 15:41:26 +0100

    made changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,7 +21,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
         <Navbar />
         <div className="flex gap-6 md:gap-20">
-          <div className=".h-[92vh].overflow-hidden.xl:hover:overflow-auto">
+          <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
             <Sidebar/>
           </div>
           <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
--- utils/client.ts
+++ utils/client.ts
@@ -1,9 +1,12 @@
-import sanityClient from '@sanity/client';
+import {createClient} from 'next-sanity';
 
-export const client = sanityClient({
-  projectId:'nrev6ch1',
-  dataset: 'production',
-  apiVersion: '2022-03-10',
-  useCdn: false,
-  token: process.env.NEXT_PUBLIC_SANITY_TOKEN, 
-});
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID // "pv8y60vp"
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET // "production"
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03'
+
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion, // https://www.sanity.io/docs/api-versioning
+  useCdn: false, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
+})
